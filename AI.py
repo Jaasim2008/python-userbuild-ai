63d752892ec82ea31ce2ec5b3033a662cccd5333
Jaasim2008 <jazztgblive@gmail.com>
2022-03-15 18:57:58 +0400

    first commit

---
+++ AI.py
@@ -0,0 +1,13 @@
+import random
+
+times_tried = 1
+while True:
+    my_number = 46
+    random_number = random.randint(0, 100)
+    if int(random_number) == my_number:
+        print("Got Number")
+        break
+    times_tried = times_tried + 1
+print(f'It Took {times_tried} Times to Find the Number')
+
+# First Attempt -> It Took 200 ~ 250 Tries To Find the Number
